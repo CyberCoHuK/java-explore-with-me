f4b029d2a55c16a4284896c7a67d7785e4bbb255
CyberCoHuK <cybercohuk@yandex.ru>
2023-08-26 14:01:44 +0300

    try to fix lazy

--- ewm-stat-service/stats-client/src/main/java/ru/practicum/stats_client/client/StatClient.java
+++ ewm-stat-service/stats-client/src/main/java/ru/practicum/stats_client/client/StatClient.java
@@ -11,7 +11,6 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.dto.EndpointHitDto;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
