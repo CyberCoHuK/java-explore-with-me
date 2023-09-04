98247179f8b78a21d15e450cbc8f844c84e6d566
CyberCoHuK <cybercohuk@yandex.ru>
2023-09-04 20:14:50 +0300

    save third

--- ewm-service/src/main/java/ru/practicum/ewm_service/events/mapper/LocationMapper.java
+++ ewm-service/src/main/java/ru/practicum/ewm_service/events/mapper/LocationMapper.java
@@ -5,14 +5,14 @@ import ru.practicum.ewm_service.events.model.Location;
 
 public class LocationMapper {
 
-    public Location toLocation(LocationDto locationDto) {
+    public static Location toLocation(LocationDto locationDto) {
         return Location.builder()
                 .lon(locationDto.getLon())
                 .lat(locationDto.getLat())
                 .build();
     }
 
-    public LocationDto toLocationDto(Location location) {
+    public static LocationDto toLocationDto(Location location) {
         return LocationDto.builder()
                 .lat(location.getLat())
                 .lon(location.getLon())
